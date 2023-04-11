0d13b6728c962fe36faf046846c1abb585a1008d
Nayan <kunayan@deloitte.com>
2023-04-11 17:33:56 +0530

    update

---
+++ src/components/header.js
@@ -0,0 +1,55 @@
+// import { Link } from "react-router-dom";
+// import React from "react";
+
+// const Header = () => {
+
+//     return (
+//         <div>
+//             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+//             {/* <nav className="navbar-dark"> */}
+//                 <div className="container-fluid">
+//                     <a className="navbar-brand" href="/">
+//                         <img
+//                             src="https://cdn.theforage.com/vinternships/companyassets/m7GusTmDpDcGMcp28/a7QBYATxBtRiExiZ8/1625078690042/DEL_SEC_RGB_aw%20(White%20Logo)v2.png"
+//                             height="22px" alt="Deloitte" />
+//                     </a>
+//                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+//                         <span className="navbar-toggler-icon"></span>
+//                     </button>
+//                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//                         <li class="nav-item">
+//                          <a class="nav-link" href="#">Link</a>
+//         </li>
+//                             <li className="nav-item">
+//                                 <Link className="nav-link" to='/blogs'>Blogs</Link>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <Link className="nav-link" to='/parent'>Parent</Link>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <Link className="nav-link" to='/blog'>OneBlog</Link>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <Link className="nav-link" to='/login'>Login</Link>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <Link className="nav-link" to='/Register'>Register</Link>
+//                             </li>
+
+//                             <li className="nav-item">
+//                                 <Link className="nav-link" to='/write'>Write Blog</Link>
+//                             </li>
+//                         </ul>
+//                         <form className="d-flex" role="search">
+//                             <input className="form-control me-2" type="search" placeholder="Search a blog..." />
+//                             <button className="btn btn-outline-primary" type="submit">Search</button>
+//                         </form>
+//                     </div>
+//                 </div>
+//             </nav>
+//         </div>
+//     );
+// };
+
+// export default Header;
